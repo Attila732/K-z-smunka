e49a32c9b5aa76b7d37bf598b0ac54d2fc6a64eb
Kovacs Attila <kovacs.attila478@gmail.com>
2021-03-16 13:23:10 +0100

    javítva

--- JSAlap.js
+++ JSAlap.js
@@ -1,16 +1,36 @@
+/* global ertek */
 
-function dolgozz(){
-    var ertek=document.getElementById("Adat").value;
-    if(ertek !==""){
-          alert("Szia"+ertek);
-          console.log("bármi");
+/* global ertek */
+
+var tomb = [];
+function feltolt() {
+    
+    var ertek = document.getElementById("Adat").value;
+    for (var i = 0; i < ertek; i++) {
+        tomb[i] = Math.floor(Math.random() * 10) + 1;
+    }
+}
+function dolgozz() {
+   feltolt();
+   var osszeg=0;
+    for (var i = 0; i < tomb.length; i++) {
+        osszeg+=tomb[i];
     }
     
- 
+   
+    document.getElementById("szoveg").innerHTML = tomb;   
 }
-function init(){
+
+function osszegzes(){
+    var osszeg=0;
+    for (var i = 0; i < tomb.length; i++) {
+        osszeg+=tomb[i];
+    }
+}
+
+function init() {
     console.log("Bejelenkeztem");
     console.log(document.getElementById("Adat").value);
-    document.getElementById("szamol").addEventListener("click",dolgozz);
+    document.getElementById("szamol").addEventListener("click", dolgozz);
 }
-window.addEventListener("load",init);
+window.addEventListener("load", init);
